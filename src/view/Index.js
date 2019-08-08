7cf526e5aed3580f0120e7bc995e2955ce1ecb5a
Kabir Shah <iamkbrsh@gmail.com>
2019-08-08 16:43:27 -0700

    add link to guide

--- src/view/Index.js
+++ src/view/Index.js
@@ -4,5 +4,6 @@ import "./Index.css";
 export default ({ data }) => (
 	<div class="Index">
 		<h1 class="Index-title">{data.name}</h1>
+		<p>Jump into the <a href="https://kbrsh.github.io/moon/doc/guide.html">guide</a> to get started.</p>
 	</div>
 );
