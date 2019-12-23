acba91f400650eaa73c2b0889a676b44653c4b34
Kabir Shah <iamkbrsh@gmail.com>
2019-12-22 20:36:02 -0800

    fix default alignment

--- src/view/Index.js
+++ src/view/Index.js
@@ -2,7 +2,7 @@ import Moon from "moon";
 import moonLogo from "images/moon-logo.png";
 
 export default ({ data }) => (
-	<div class="p-20 x-c s-5">
+	<div class="p-20 d-y a-m-c a-c-c s-3">
 		<img class="w-15 h-15" src={moonLogo} alt="Moon logo"/>
 		<h1>{data.name}</h1>
 		<p>Jump into the <a href="https://kbrsh.github.io/moon/doc/guide.html" target="_blank">guide</a> to get started.</p>
