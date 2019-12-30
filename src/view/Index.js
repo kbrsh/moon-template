9961ed2db718d35a57b76d8640a5f5ec09d8866d
Kabir Shah <iamkbrsh@gmail.com>
2019-12-29 19:43:43 -0800

    ensure elements layout vertically

--- src/view/Index.js
+++ src/view/Index.js
@@ -2,9 +2,9 @@ import Moon from "moon";
 import moonLogo from "images/moon-logo.png";
 
 export default ({ data }) => (
-	<div id="root" class="a-x-c p-x-20 p-y-20 m-x-3 m-y-3">
+	<div id="root" class="a-x-c p-y-20 m-x-3 m-y-3">
 		<img src={moonLogo} alt="Moon logo" class="s-x-15 s-x-15"/>
-		<h1>{data.name}</h1>
-		<p>Jump into the <a href="https://kbrsh.github.io/moon/doc/guide.html" target="_blank">guide</a> to get started.</p>
+		<h1 class="s-x-25 a-t-c">{data.name}</h1>
+		<p class="s-x-25 a-t-c">Jump into the <a href="https://kbrsh.github.io/moon/doc/guide.html" target="_blank">guide</a> to get started.</p>
 	</div>
 );
