974d04228d99752502da264c2f8d2ef9d0cca937
Kabir Shah <iamkbrsh@gmail.com>
2019-12-29 19:51:28 -0800

    remove unused style

--- src/view/Index.js
+++ src/view/Index.js
@@ -3,7 +3,7 @@ import moonLogo from "images/moon-logo.png";
 
 export default ({ data }) => (
 	<div id="root" class="a-x-c p-y-20">
-		<div class="s-x-25 a-x-c m-x-3 m-y-3">
+		<div class="s-x-25 a-x-c m-y-3">
 			<img src={moonLogo} alt="Moon logo" class="s-x-15 s-x-15"/>
 			<h1 class="s-x-25 a-t-c">{data.name}</h1>
 			<p class="s-x-25 a-t-c">Jump into the <a href="https://kbrsh.github.io/moon/doc/guide.html" target="_blank">guide</a> to get started.</p>
