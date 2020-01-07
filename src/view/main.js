7fe77d2a050448bd1ecbb7baf6d541f9054ea43f
Kabir Shah <iamkbrsh@gmail.com>
2020-01-06 22:13:39 -0800

    new formatting for function

--- src/view/main.js
+++ src/view/main.js
@@ -2,12 +2,11 @@ import Moon from "moon";
 import moonLogo from "images/moon-logo.png";
 const { div, img, h1, p, a } = Moon.view.m;
 
-export default ({ data }) => (
+export default ({ data }) =>
 	<div id="root" class="a-x-c a-y-l-c">
 		<div class="s-x-25 a-x-c m-y-3">
 			<img src=moonLogo alt="Moon logo" class="s-x-15 s-y-15"/>
 			<h1 class="s-x-25 a-t-c">{data.name}</h1>
 			<p class="s-x-25 a-t-c">Jump into the <a href="https://moonjs.org/guide" target="_blank">guide</a> to get started.</p>
 		</div>
-	</div>
-);
+	</div>;
