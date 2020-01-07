d0d26d05b6a2291bd0a2d241bfec0cf55545124d
Kabir Shah <iamkbrsh@gmail.com>
2020-01-06 22:01:46 -0800

    fix styles and image link

--- src/view/main.js
+++ src/view/main.js
@@ -1,10 +1,11 @@
 import Moon from "moon";
 import moonLogo from "images/moon-logo.png";
+const { div, img, h1, p, a } = Moon.view.m;
 
 export default ({ data }) => (
-	<div id="root" class="a-x-c p-y-20">
+	<div id="root" class="a-x-c a-y-l-c">
 		<div class="s-x-25 a-x-c m-y-3">
-			<img src={moonLogo} alt="Moon logo" class="s-x-15 s-x-15"/>
+			<img src=moonLogo alt="Moon logo" class="s-x-15 s-x-15"/>
 			<h1 class="s-x-25 a-t-c">{data.name}</h1>
 			<p class="s-x-25 a-t-c">Jump into the <a href="https://kbrsh.github.io/moon/doc/guide.html" target="_blank">guide</a> to get started.</p>
 		</div>
